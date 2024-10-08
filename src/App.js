e149ff26303b8a9071687130c71da02beab444ef
Analita <88949609+warlita@users.noreply.github.com>
2024-10-07 20:00:59 -0500

    adding bootstrap to app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import Home from './Home';
 import Movie from './Movie';
 
